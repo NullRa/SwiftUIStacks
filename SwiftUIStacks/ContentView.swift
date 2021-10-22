fdb2426aef8fcb3b26e1f150a0e3b47a7f73cd8c
jyang <jTang@walkgame.com>
2021-10-22 15:42:38 +0800

    VStack用來垂直排列視圖, aligment參數可以設定視圖的對齊方式, spacing設定試圖間的間距

--- SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 2) {
+            Text("Choose")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+            Text("Your Plan")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+        }
     }
 }
 
